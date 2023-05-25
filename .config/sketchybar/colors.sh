59c7ce9fb746a86d844e51ed412da1e3a8800297
itrajkov <ivche1337@outlook.com>
2023-05-25 18:10:44 +0200

    Add macos configs.

---
+++ .config/sketchybar/colors.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# Color Palette
+BLACK=0x00000050
+WHITE=0xFFFFFFFF
+RED=0xffed8796
+GREEN=0xffa6da95
+BLUE=0xff8aadf4
+YELLOW=0xffeed49f
+ORANGE=0xfff5a97f
+MAGENTA=0xffc6a0f6
+GREY=0xff939ab7
+TRANSPARENT=0x00000000
+
+# General bar colors
+# BAR_COLOR=0xcc24273a #$GREY # Grey bar
+BAR_COLOR=$TRANSPARENT # Grey bar
+ICON_COLOR=$WHITE # Color of all icons
+LABEL_COLOR=$GREY # Color of all labels
+
+POPUP_BACKGROUND_COLOR=$BLACK
+POPUP_BORDER_COLOR=$WHITE
+
+SHADOW_COLOR=$BLACK
+
+# Item specific special colors
+SPOTIFY_GREEN=$GREEN
