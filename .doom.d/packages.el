a3f6fac95036a1b1a0d7cb26e57d319a80fb9fde
itrajkov <ivche1337@outlook.com>
2023-05-25 17:12:20 +0200

    Update doom config.

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,17 +1,10 @@
-;; Treesitter
-(package! tree-sitter)
-(package! tree-sitter-langs)
+(package! lsp-tailwindcss :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 
 ;; App packages
 (package! erc-hl-nicks)
 (package! erc-nick-notify)
 
-;; Org packages
-(package! org-super-agenda)
-(package! org-wild-notifier)
-
 ;; Tools
 (package! nvm)
-
-(unpin! org-roam)
 (package! org-roam-ui)
+(package! elcord :pin "d0c9ace493d088bc70f7422705ff27dfcf162cca")
