4897ac0a153f29d7037fba38175ff97dfe19db34
itrajkov <ivche1337@outlook.com>
2023-09-24 18:35:09 +0200

    Cleanup unnecessary configs.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -166,7 +166,7 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       (python +lsp +tree-sitter +pylsp +poetry +pyenv)            ; beautiful is better than ugly
+       (python +lsp +tree-sitter +pylsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
