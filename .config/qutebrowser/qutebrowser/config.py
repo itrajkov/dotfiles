add5fdb09014162b564fa5b17e7ece8f0d275016
Ivan Trajkov <ivche@trajkov.mk>
2024-03-29 00:06:37 +0100

    Update qutebrowser, add fuzzel, fix .zprofile.

---
+++ .config/qutebrowser/qutebrowser/config.py
@@ -0,0 +1,21 @@
+config.load_autoconfig()
+
+c.content.private_browsing = False
+c.content.javascript.clipboard = True
+
+# c.fonts.tabs.selected = '14pt default_family'
+c.fonts.default_family = 'Dina Remaster'
+c.fonts.default_size = '14pt'
+
+
+# dark mode
+# c.colors.webpage.preferred_color_scheme = 'dark'
+# c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+# c.colors.webpage.darkmode.contrast = 0.0
+# c.colors.webpage.darkmode.enabled = True
+# c.colors.webpage.darkmode.grayscale.all = False
+# c.colors.webpage.darkmode.grayscale.images = 0.0
+# c.colors.webpage.darkmode.policy.images = 'smart'
+# c.colors.webpage.darkmode.policy.page = 'smart'
+# c.colors.webpage.darkmode.threshold.background = 128
+# c.colors.webpage.darkmode.threshold.text = 128
